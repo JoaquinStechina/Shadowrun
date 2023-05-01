0c5c00f43ff09bb2b5cc8f8bf048ca829d55fea7
JoaquinStechina <joaquinstechina12@gmail.com>
2023-05-01 03:01:42 -0300

    Se comienza a vincular la API con el Front

---
+++ src/data/db.js
@@ -0,0 +1,34 @@
+export const db = [
+    {
+        id:1,
+        nombre:"Getting Started",
+    },
+    {
+        id:2,
+        nombre:"Core Rulebooks",
+    },
+    {
+        id:3,
+        nombre:"Core Supplements",
+    },
+    {
+        id:4,
+        nombre:"Sourcebooks",
+    },
+    {
+        id:5,
+        nombre:"Setting Supplemental",
+    },
+    {
+        id:6,
+        nombre:"Enhanced Fiction",
+    },
+    {
+        id:7,
+        nombre:"Adventures",
+    },
+    {
+        id:8,
+        nombre:"Campaigns"
+    }
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,20 @@
-export default function Home() {
+export default function Home({datos}) {
   return (
     <main>
+      {datos.map(dato=><h2 key={dato.id}>{dato.nombre}</h2>)}
     </main>
   )
+}
+
+export async function getStaticProps() {
+  // Hacer una solicitud a la API
+  const res = await fetch('http://localhost:3000/api/categories')
+  const datos = await res.json()
+
+  // Devolver los datos como props
+  return {
+    props: {
+      datos
+    }
+  }
 }
