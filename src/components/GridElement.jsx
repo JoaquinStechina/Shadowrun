67f74486a3ece9f63935a28f14c55c866c74d37a
JoaquinStechina <joaquinstechina12@gmail.com>
2023-05-07 02:30:58 -0300

    Modified the amount the background position can change, changed background color, now the cursor doesn't change when it hovers over the h2

--- src/components/GridElement.jsx
+++ src/components/GridElement.jsx
@@ -6,12 +6,11 @@ export default function GridElement({dato}){
     const mouseMove = (e) =>{
       const x = e.clientX;
       const y = e.clientY;
-      const newX = (x / window.innerWidth) * 100;
-      const newY = (y / window.innerHeight) * 100;
-
-      setTimeout(() => {
+      const newX = (x / (window.innerWidth * 3)) * 100;
+      const newY = (y / (window.innerHeight * 3)) * 100;
+      setTimeout(()=>{
         setBackgroundPosition(`${newX}% ${newY}%`);
-      }, 100);
+      },50)
     }
 
     return(
@@ -19,11 +18,11 @@ export default function GridElement({dato}){
           onMouseMove={mouseMove}
           className="border-4 border-[#133e7c] shadow-[0_0_8px_#0abdc6] group rounded-lg w-[200px] h-[250px] relative" 
         >
-          <div className="overflow-hidden saturate-50 transition-[opacity] duration-500 group-hover:opacity-80 w-full h-full bg-[size:150%]" 
+          <div className="overflow-hidden saturate-50 transition-[opacity] duration-[200ms] ease-out group-hover:opacity-80 w-full h-full bg-[size:150%]" 
                 style={{backgroundImage:`url(${dato.imagen})`, backgroundPosition:`${backgroundPosition}`}}
           />
-          <div className="absolute group-hover:opacity-100 group-hover:backdrop-blur-sm border-2 border-[#711c91] rounded-lg transition-[opacity backdrop-filter] duration-500 group-hover:animate-[pink-flicker_1.7s_cubic-bezier(.52,0,.54,1)_infinite] opacity-0 w-4/5 h-4/5 right-[10%] top-[10%] text-center">
-            <h2 className="">{dato.nombre}</h2>
+          <div className="absolute group-hover:opacity-100 group-hover:backdrop-blur-sm border-2 border-[#711c91] rounded-lg transition-[opacity backdrop-filter] duration-500 group-hover:animate-[pink-flicker_1.7s_cubic-bezier(.52,0,.54,1)_infinite] opacity-0 w-4/5 h-4/5 right-[10%] top-[10%] flex items-center justify-center text-center">
+            <h2 className="cursor-default text-white text-2xl after">{dato.nombre}</h2>
           </div>
         </div>
       )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home({datos}) {
 
   return (
     <main>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center min-w-full gap-y-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 justify-items-center min-w-full gap-y-6">
         {datos.map(dato => {
           return(
             <GridElement dato={dato}/>
